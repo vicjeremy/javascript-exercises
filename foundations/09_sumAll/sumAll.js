4ccaf46ed04a40e9e1e20700d97d3b564210883d
Vic Jeremy <162003469+vicjeremy@users.noreply.github.com>
2026-03-02 13:53:11 +0700

    Complete exercise foundations 06 - 11

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0
+    if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0){
+        return "ERROR"
+    } else if (start > end) {
+        for (let i = end; i < start + 1; i++) {
+          sum += i;
+        }
+    } else{
+        for (let i = start; i < end + 1; i++) {
+          sum += i;
+        }
+    }
 
+      
+    return sum
 };
 
 // Do not edit below this line
--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+const freezingPoint = 32;
+const convertToCelsius = function (tempFahrenheit) {
+  const ratioCelsius = 5 / 9;
+  let result = (tempFahrenheit - freezingPoint) * ratioCelsius;
+  const roundedOneDecimal = Math.round(result * 10) / 10;
+  return roundedOneDecimal;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempCelsius) {
+  const ratioFahrenheit = 9 / 5
+  let result = ratioFahrenheit * tempCelsius + freezingPoint; ;
+  const roundedOneDecimal = Math.round(result * 10) / 10;
+  return roundedOneDecimal;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
